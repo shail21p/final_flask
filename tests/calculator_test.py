8508f8b0b53c256b593ce99e58c5d5dbd1f9dda1
shail patel <sp233@njit.edu>
2022-05-06 19:55:01 -0400

    tests

---
+++ tests/calculator_test.py
@@ -0,0 +1,21 @@
+"""Testing the Calculator"""
+# From specifies the namespace
+from calculator import Calculator
+
+
+def test_calculator_add_method():
+    """Testing the Calculator Add"""
+    # this is show using the calculator object add method
+    tuple_list = (1,2)
+    assert Calculator.add(tuple_list) == 3
+
+def test_calculator_subtract_method():
+    """Testing the Calculator Subtract"""
+    tuple_list = (1,2)
+    assert Calculator.subtract(tuple_list) == -3
+
+
+def test_calculator_multiply_method():
+    """Testing the Calculator Multiply"""
+    tuple_list = (1,2)
+    assert Calculator.multiply(tuple_list) == 2
