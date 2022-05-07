a4b55d8406a56e91d8f7ca29008371f8610b52c2
shail patel <sp233@njit.edu>
2022-05-06 21:31:16 -0400

    fixes

--- app/simple_pages/__init__.py
+++ app/simple_pages/__init__.py
@@ -2,13 +2,26 @@ from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
 
 simple_pages = Blueprint('simple_pages', __name__,
-                        template_folder='templates',static_folder='static')
+                        template_folder='templates')
 
 
-@simple_pages.route('/', defaults={'page': 'index'})
-@simple_pages.route('/<page>')
-def show(page):
+@simple_pages.route('/')
+def index():
     try:
-        return render_template('%s.html' % page)
+        return render_template('index.html')
+    except TemplateNotFound:
+        abort(404)
+
+@simple_pages.route('/about')
+def about():
+    try:
+        return render_template('about.html')
+    except TemplateNotFound:
+        abort(404)
+
+@simple_pages.route('/welcome')
+def welcome():
+    try:
+        return render_template('welcome.html')
     except TemplateNotFound:
         abort(404)
