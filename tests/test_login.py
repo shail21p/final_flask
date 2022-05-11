228b59d42cd0eba3ff8552ec27a484d6a0fb75a4
sp233 <sp233@njit.edu>
2022-05-11 23:11:06 +0700

    push test

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,4 +1,27 @@
-
-
+import pytest
+import requests as client
 def test_uppercase():
     assert "loud noises".upper() == "LOUD NOISES"
+
+# @pytest.fixture
+def test_register():
+    response = client.post("http://localhost:5000/register", data={
+        "email": "akate@gmail.com",
+        "password": "akate",
+    })
+    assert response.status_code == 200
+
+
+# @pytest.fixture
+def test_login():
+    response = client.post("http://localhost:5000/login", data={
+        "email": "akate@gmail.com",
+        "password": "akate",
+    })
+    assert response.status_code == 200
+
+
+# @pytest.fixture
+def test_logout():
+    response = client.post('http://localhost:5000/logout')
+    assert response.status_code == 200
