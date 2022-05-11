98da2fc8fc1a3b52d31318f80053880f4eb28b62
sp233 <sp233@njit.edu>
2022-05-11 08:26:40 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,13 +8,13 @@ Create an heroku app, if no arguments are provided, heroku assigns random name t
 # heroku create
 
 This is the most important step. NAVIGATE TO THE PATH WHERE “Dockerfile” is located, this will build the image and push the image to heroku registry.
-# heroku container:push web -a <name of the heroku app>
+heroku container:push web -a flask-app-shailpatel815
 
 Creating the container on heroku host and hosting it publicly
 # heroku container:release web -a flask-app-shailpatel815
 
 To open the app in your default browser
-# heroku open -a <name of heroku app>
+heroku open -a flask-app-shailpatel815
 
 You can check the logs of that particular heroku app
 # heroku logs --tail -a <name of heroku app>
